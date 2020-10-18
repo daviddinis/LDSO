381f6d6cf405f34d21b7f6b5a74a22a6deb05713
Gustavo Tavares <up201700129@fe.up.pt>
2020-10-18 11:09:52 +0000

    Add Driver

---
+++ skeleton/resources/views/pages/addDriver.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+
+@section('content')
+<form class="text-black mw-50" method="post" action="{{route('driver.store')}}" >
+    {{ method_field('POST') }}
+    {{ csrf_field() }}
+
+    <label class="">Name:<input id="name" type="text" name="name" class="" required></label>
+    <label class="">Email:<input id="email" type="email" name="email" class="" required></label>
+    <label class="">Driver License:<input id="license" type="text" name="license" class="" required></label>
+    <button type=" submit" class="btn bg-mydarkgreen text-white mt-1 ml-auto">Submit</button>
+</form>
+
+@endsection
+
