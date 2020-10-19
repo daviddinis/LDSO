95c5dc36168a12c9ffe21eff778ce5a0c73fa3bd
david <up201706766@fe.up.pt>
2020-10-19 21:02:11 +0100

    fixed minor route problem

--- fleetmanagement/routes/web.php
+++ fleetmanagement/routes/web.php
@@ -31,9 +31,6 @@ Route::post('login', 'Auth\LoginController@login');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('register', 'Auth\RegisterController@register');
-<<<<<<< HEAD
-=======
 
 //Driver
 Route::resource('driver', 'DriverController');
->>>>>>> origin/development
