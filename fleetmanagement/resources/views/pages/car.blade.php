ad5dc4a5278a9b7aadbbba9f1ccc7837b72ae088
André Mamprin Mori <up201700493@fe.up.pt>
2020-10-19 17:12:33 +0100

    Added function to sum all the costs of the car

--- fleetmanagement/resources/views/pages/car.blade.php
+++ fleetmanagement/resources/views/pages/car.blade.php
@@ -3,4 +3,22 @@
 @section('title', 'Car')
 
 @section('content')
+<h1>{{$car->make}} {{$car->model}} - {{$car->license_plate}}</h1>
+<h2>Total cost:
+    @php
+    function sum_costs($arr){
+        $total = 0;
+        foreach ($arr as $tax) {
+            $total += $tax['value'];
+        }
+        return $total;
+    }
+    echo sum_costs($car->taxes) + sum_costs($car->maintenances) + sum_costs($car->inspections) + sum_costs($car->insurances);
+    @endphp
+    €
+</h2>
+<h4>Maintenances: {{$car->maintenances}}</h4>
+<h4>Insurances: {{$car->insurances}}</h4>
+<h4>Taxes: {{$car->taxes}}</h4>
+<h4>Inspections: {{$car->inspections}}</h4>
 @endsection
