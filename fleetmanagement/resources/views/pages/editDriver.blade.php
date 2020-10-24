aba4a2a9023a1fe8df57af8657e2327a21a1a3df
André Mamprin Mori <up201700493@fe.up.pt>
2020-10-24 18:07:55 +0100

    Removed commented code

--- fleetmanagement/resources/views/pages/editDriver.blade.php
+++ fleetmanagement/resources/views/pages/editDriver.blade.php
@@ -2,16 +2,6 @@
 
 
 @section('content')
-{{-- <form class="text-black mw-50" method="post" action="{{route('driver.update', $driver->id)}}" >
-    {{ method_field('PUT') }}
-    {{ csrf_field() }}
-
-    <label class="">Name:<input id="name" type="text" name="name" class="" value={{$driver->name}} required></label>
-    <label class="">Email:<input id="email" type="email" name="email" class="" value={{$driver->email}}></label>
-    <label class="">Driver License:<input id="drivers_license" type="text" name="drivers_license" class="" value={{$driver->drivers_license}}></label>
-    <label class="">Identification Card:<input id="id_card" type="number" name="id_card" class="" value={{$driver->id_card}}></label>
-    <button type=" submit" class="btn bg-mydarkgreen text-white mt-1 ml-auto">Submit</button>
-</form> --}}
 <div class="container-md">
     <form method="POST" action="{{route('driver.update', $driver->id)}}">
         {{ method_field('PUT') }}
