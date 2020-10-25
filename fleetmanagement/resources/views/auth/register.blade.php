9db084441fcb7c032ecc36613f43e4e2fe46ec7e
André Mamprin Mori <up201700493@fe.up.pt>
2020-10-25 18:30:53 +0000

    Updated register form and backend

--- fleetmanagement/resources/views/auth/register.blade.php
+++ fleetmanagement/resources/views/auth/register.blade.php
@@ -1,39 +1,64 @@
 @extends('layouts.app')
 
 @section('content')
-<form method="POST" action="{{ route('register') }}">
-    {{ csrf_field() }}
-
-    <label for="name">Name</label>
-    <input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus>
-    @if ($errors->has('name'))
-      <span class="error">
-          {{ $errors->first('name') }}
-      </span>
-    @endif
-
-    <label for="email">E-Mail Address</label>
-    <input id="email" type="email" name="email" value="{{ old('email') }}" required>
-    @if ($errors->has('email'))
-      <span class="error">
-          {{ $errors->first('email') }}
-      </span>
-    @endif
-
-    <label for="password">Password</label>
-    <input id="password" type="password" name="password" required>
-    @if ($errors->has('password'))
-      <span class="error">
-          {{ $errors->first('password') }}
-      </span>
-    @endif
-
-    <label for="password-confirm">Confirm Password</label>
-    <input id="password-confirm" type="password" name="password_confirmation" required>
-
-    <button type="submit">
-      Register
-    </button>
-    <a class="button button-outline" href="{{ route('login') }}">Login</a>
-</form>
+<div class="container-md">
+    <form method="POST" action="{{ route('register') }}">
+        {{ csrf_field() }}
+        <div class="row" style="margin-top: 5%">
+            <div class="col-md-2"></div>
+            <div class="col">
+                <h1>Register</h1>
+            </div>
+        </div>
+        <br>
+        <div class="row form-group">
+            <div class="col-md-2"></div>
+            <div class="col-md-4">
+                <label for="name">Name</label>
+                <div class="input-group">
+                    <input type="text" class="form-control" id="name" name="name" required>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <label for="name">Email</label>
+                <div class="input-group">
+                    <input type="email" class="form-control" id="email" name="email" required>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row form-group">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                <label for="name">Company Name</label>
+                <div class="input-group">
+                    <input type="text" class="form-control" id="company_name" name="company_name" required>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row form-group">
+            <div class="col-md-2"></div>
+            <div class="col-md-4">
+              <label for="name">Password</label>
+              <div class="input-group">
+                  <input type="password" class="form-control" id="password" name="password" required>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <label for="name">Confirm Password</label>
+              <div class="input-group">
+                  <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
+              </div>
+            </div>
+        </div>
+        <br>
+        <div class="row form-group" style="margin-bottom: 5%">
+            <div class="col-md-2"></div>
+            <div class="col-md-2">
+                <button type="submit" class="btn btn-primary btn-lg">Create</button>
+            </div>
+        </div>
+    </form>
+</div>
 @endsection
