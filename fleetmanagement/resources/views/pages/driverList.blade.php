b2deb4feef910f064957833e37967e14ab02caf8
Gustavo Tavares <up201700129@fe.up.pt>
2020-10-26 11:02:59 +0000

    driver design

--- fleetmanagement/resources/views/pages/driverList.blade.php
+++ fleetmanagement/resources/views/pages/driverList.blade.php
@@ -1,24 +1,16 @@
 @extends('layouts.app')
 
-
 @section('content')
-<h2> Driver List </h2>
-@if (count($drivers) > 0)
-        @foreach ($drivers as $driver)
-                <div class="new_item">
-                <strong>Name:</strong> {{$driver->name}} <br>
-                <strong>Email:</strong> {{$driver->email}} <br>
-                <strong>Driver License:</strong> {{$driver->drivers_license}} <br>
-                <strong>Identification Card:</strong> {{$driver->id_card}} <br>
-                <button onclick="window.location.href='/driver/{{$driver->id}}/edit'"> Edit </button> <br> <br>
-                <form class="text-black mw-50" method="post" action="{{route('driver.destroy', $driver->id)}}" >
-                    {{ method_field('DELETE') }}
-                    {{ csrf_field() }}
-                    <button class="" type=" submit">Delete</button>
-                </form>
-        @endforeach
-@endif
 
-<button onclick="window.location.href='/driver/create'"> add Driver</button>
+<section id="driver" class="d-flex flex-wrap justify-content-around align-content-around mt-5">
+
+    @each('partials.driver', $drivers, 'driver')
+
+
+
+</section> 
+<a href="/driver/create" class="btn btn-primary btn-lg rounded-circle " style="position:absolute;bottom:30px;right:30px;">
+    <i class="fa fa-plus"></i>
+</a>
 
 @endsection
