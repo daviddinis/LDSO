4a487af351abfb3096f4125fc050233978e6dcb4
Luywer <luisafonsoso@gmail.com>
2020-11-08 22:55:36 +0000

    fixed alertCards responsive design

--- fleetmanagement/resources/views/partials/alertCards.blade.php
+++ fleetmanagement/resources/views/partials/alertCards.blade.php
@@ -1,14 +1,14 @@
 <div class="alertCardsSection row">
-    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+    <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
         @include('partials.alertCard', ['category' => 'Insurance', 'alertTimes' => $alertTimez, 'eventDate' => $insuranceDate])
     </div>
-    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+    <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
         @include('partials.alertCard', ['category' => 'Tax', 'alertTimes' => $alertTimez, 'eventDate' => $taxDate])
     </div>
-    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+    <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
         @include('partials.alertCard', ['category' => 'Inspection', 'alertTimes' => $alertTimez, 'eventDate' => $inspectionDate])
     </div>
-    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+    <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
         @include('partials.alertCard', ['category' => 'Maintenance', 'alertTimes' => $alertTimez, 'eventDate' => $maintenanceDate])
     </div>
 </div>
