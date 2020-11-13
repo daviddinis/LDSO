c18c6061edbfb4bc0c6d5172a33151ffa423b9dd
Maria <mariagcaldeira99@gmail.com>
2020-11-13 01:16:31 +0000

    Fixing Design

--- fleetmanagement/resources/views/pages/tax.blade.php
+++ fleetmanagement/resources/views/pages/tax.blade.php
@@ -1,49 +1,61 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-md">
-        {{ csrf_field() }}
-        <div class="row" style="margin-top: 5%">
-            <div class="col-md-2"></div>
-            <div class="col">
-                <h1>Most Recent Tax</h1>
+<div class="jumbotron" style="padding-top: 3%;padding-bottom: 20%">
+    <ol class="breadcrumb" style="border-width: 0">
+        <li class="breadcrumb-item"><a href="{{route('car.show', $car->id)}}">
+                <p>{{$car->make}} {{$car->model}} - {{$car->license_plate}}</p>
+            </a></li>
+
+        <li class="breadcrumb-item active"><a href="{{route('tax.find', $car->id)}}">
+            <p>Taxes</p>
+        </a></li>
+        <li class="breadcrumb-item active">Tax {{$tax->id}}</li>
+    </ol>
+    <div class="container-md">
+            {{ csrf_field() }}
+            <div class="row" style="margin-top: 5%">
+                <div class="col-md-2"></div>
+                <div class="col">
+                    <h1>Tax {{$tax->id}}</h1>
+                </div>
             </div>
-        </div>
-        <br>
-        <div class="row form-group">
-            <div class="col-md-2"></div>
-            <div class="col-md-4">
-                <h5>Date of the most recent tax: {{$tax->date}}</h5>
+            <br>
+            <div class="row form-group">
+                <div class="col-md-2"></div>
+                <div class="col-md-8">
+                    <h5><strong>Date of the most recent tax: </strong>{{$tax->date}}</h5>
+                </div>
             </div>
-        </div>
-        <br>
-        <div class="row form-group">
-            <div class="col-md-2"></div>
-            <div class="col-md-8">
-            <h5>Expiration date of the tax: {{$tax->expiration_date}}</h5>
+            <br>
+            <div class="row form-group">
+                <div class="col-md-2"></div>
+                <div class="col-md-8">
+                <h5><strong>Expiration date of the tax: </strong>{{$tax->expiration_date}}</h5>
+                </div>
             </div>
-        </div>
-        <br>
-        <div class="row form-group">
-            <div class="col-md-2"></div>
-            <div class="col-md-4">
-            <h5>Tax value: {{$tax->value}}€</h5>
+            <br>
+            <div class="row form-group">
+                <div class="col-md-2"></div>
+                <div class="col-md-4">
+                <h5><strong>Tax value: </strong>{{$tax->value}}€</h5>
+                </div>
             </div>
-        </div>
-        <br>
-        <div class="row form-group">
-            <div class="col-md-2"></div>
-            <div class="col-md-4">
-            <h5>File: @if($tax->file != null) <a href="{{ asset($tax->file) }}" style="color: black" download="{{substr($tax->file, 17)}}">Download File</a> @else N/A @endif</h5>
+            <br>
+            <div class="row form-group">
+                <div class="col-md-2"></div>
+                <div class="col-md-4">
+                <h5><strong>File: </strong>@if($tax->file != null) <a href="{{ asset($tax->file) }}" style="color: black" download="{{substr($tax->file, 17)}}">Download File</a> @else N/A @endif</h5>
+                </div>
             </div>
-        </div>
-        <br>
-        <div class="row form-group">
-            <div class="col-md-2"></div>
-            <div class="col-md-4">
-            <h5>Observations: {{$tax->obs}}</h5>
+            <br>
+            <div class="row form-group">
+                <div class="col-md-2"></div>
+                <div class="col-md-4">
+                <h5><strong>Observations: </strong>{{$tax->obs}}</h5>
+                </div>
             </div>
-        </div>
-        <br>
+            <br>
+    </div>
 </div>
 @endsection
--- fleetmanagement/resources/views/pages/taxes.blade.php
+++ fleetmanagement/resources/views/pages/taxes.blade.php
@@ -14,49 +14,49 @@
     <div class="row" style="margin-top: 5%">
     <div class="col-md-2"></div>
         <div class="col">
-            <h1>Most Recent Tax</h1>
+            <h1>Most Recent Tax Added</h1>
         </div>
     </div>
     <br>
     <div class="row form-group">
         <div class="col-md-2"></div>
         <div class="col-md-8">
-            <h5>Date of the most recent tax: {{$car->taxes->last()->date}}</h5>
+            <h5><strong>Date of the most recent tax: </strong> {{$car->taxes->sortByDesc('id')->first()->date}}</h5>
         </div>
     </div>
     <br>
     <div class="row form-group">
         <div class="col-md-2"></div>
         <div class="col-md-8">
-        <h5>Expiration date of the tax: {{$car->taxes->last()->expiration_date}}</h5>
+        <h5><strong>Expiration date of the tax: </strong>{{$car->taxes->sortByDesc('id')->first()->expiration_date}}</h5>
         </div>
     </div>
     <br>
     <div class="row form-group">
         <div class="col-md-2"></div>
         <div class="col-md-4">
-        <h5>Tax value: {{$car->taxes->last()->value}}€</h5>
+        <h5><strong>Tax value:</strong> {{$car->taxes->sortByDesc('id')->first()->value}}€</h5>
         </div>
     </div>
     <br>
     <div class="row form-group">
         <div class="col-md-2"></div>
         <div class="col-md-4">
-        <h5>File: @if($car->taxes->last()->file != null) <a href="{{ asset($car->taxes->last()->file) }}" style="color: black" download="{{substr($car->taxes->last()->file, 17)}}">Download File</a> @else N/A @endif</h5>
+        <h5><strong>File:</strong> @if($car->taxes->sortByDesc('id')->first()->file != null) <a href="{{ asset($car->taxes->sortByDesc('id')->first()->file) }}" style="color: black" download="{{substr($car->taxes->sortByDesc('id')->first()->file, 17)}}">Download File</a> @else N/A @endif</h5>
         </div>
     </div>
     <br>
     <div class="row form-group">
         <div class="col-md-2"></div>
         <div class="col-md-4">
-        <h5>Observations: {{$car->taxes->last()->obs}}</h5>
+        <h5><strong>Observations: </strong>{{$car->taxes->sortByDesc('id')->first()->obs}}</h5>
         </div>
     </div>
     <br>
 
     <table class="table table-hover">
         <thead>
-            <tr >
+            <tr class="table-active">
                 <th scope="col">ID</th>
                 <th scope="col">Date</th>
                 <th scope="col">Expiration Date</th>
@@ -75,14 +75,14 @@
                 <td>{{$tax->value}}€</td>
                 <td>@if($tax->file != null) <a href="{{ asset($tax->file) }}" style="color: white" download="{{substr($tax->file, 17)}}">Download File</a> @else N/A @endif </td>
                 <td>@if($tax->obs != null){{$tax->obs}} @else N/A @endif</td>
-                <td>
-                    <form style="margin-left: 10px; float: right;" method="post" action="{{route('tax.destroy', [$car->id, $tax->id])}}" >
+                <td >
+                    <form style="margin-right: 50px; float: right;" method="post" action="{{route('tax.destroy', [$car->id, $tax->id])}}" >
                         {{ method_field('DELETE') }}
                         {{ csrf_field() }}
-                        <button onclick="return confirm('Are you sure?')" class="btn btn-secondary btn-sm rounded-circle "><i class="fa fa-trash"></i></button>
+                        <button onclick="return confirm('Are you sure?')" class="btn btn-danger btn-sm rounded-circle "><i class="fa fa-trash"></i></button>
                     </form>
 
-                    <a href= {{route('tax.edit', [$car->id, $tax])}} class="btn btn-info btn-sm rounded-circle" style="float:right"><i class="fa fa-pencil"></i></a>
+                    <a href= {{route('tax.edit', [$car->id, $tax])}} class="btn btn-info btn-sm rounded-circle" style="margin-right: -50px;"><i class="fa fa-pencil"></i></a>
                 </td>
             </tr>
             @endforeach
