9b22186dff27efdd9706a1aafb929496a6eb3ca2
david <up201706766@fe.up.pt>
2020-11-23 21:17:48 +0000

    add grid/list toggable views in the dashboard

--- fleetmanagement/resources/views/pages/cars.blade.php
+++ fleetmanagement/resources/views/pages/cars.blade.php
@@ -1,15 +1,40 @@
 @extends('layouts.app')
 
+@section('scripts')
+<script src="{{ asset('js/dashBoardPage.js') }}" rel="stylesheet" defer></script>
+@endsection
+
 @section('content')
 
-<section id="cars" class="d-flex flex-wrap justify-content-around align-content-around mt-5">
+<section id="cars">
+    <div class="d-flex justify-content-between my-2 ">
+        <h2 class="">Dashboard </h2><button onclick="gridViewToggle()"class="btn btn-outline-primary" ><i class="fa fa-list p-1" id="gridViewToggle" ></i></button>
+    </div>
+    <div class="d-none" id="gridView">
+        <div class="d-flex flex-wrap justify-content-around align-content-around ">
+            @each('partials.carGrid', $cars, 'car')
+        </div>
+    </div>
+    <div class="" id="listView">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">Model</th>
+                    <th scope="col">License PLate</th>
+                    <th scope="col">Issues</th>
+                    <th scope="col">Current Driver</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @each('partials.carList', $cars, 'car')
+            </tbody>
+        </table>
+    </div>
 
-    @each('partials.car', $cars, 'car')
 
-
-
-</section> 
-<a href="/car/create" class="btn btn-primary btn-lg rounded-circle " style="position:absolute;bottom:30px;right:30px;">
+</section>
+<a href="/car/create" class="btn btn-primary btn-lg rounded-circle " style="position:fixed;bottom:30px;right:30px;">
     <i class="fa fa-plus"></i>
 </a>
 
---
+++ fleetmanagement/resources/views/partials/carList.blade.php
@@ -0,0 +1,61 @@
+@if($car->issues() == 0)
+<tr class="">
+    @elseif($car->issues() == 1)
+<tr class="">
+    @else
+<tr class="">
+    @endif
+    <th scope="row">{{ $car->make }} {{ $car->model }}</th>
+    <td>{{ $car->license_plate }}</td>
+    <td>
+        @if($car->issues() == 0)
+        <span class="badge badge-primary badge-pill">0</span>
+        @elseif($car->issues() == 1)
+        <span class="badge badge-warning badge-pill">1</span>
+        @else
+        <span class="badge badge-danger badge-pill">{{$car->issues()}}</span>
+        @endif</td>
+    <td>{{$car->currentDriver()}}</td>
+    <th>
+        <form style="float:right" method="post" action="{{route('car.destroy', $car->id)}}">
+            {{ method_field('DELETE') }}
+            {{ csrf_field() }}
+            <button onclick="return confirm('Are you sure?')" class="btn btn-secondary btn-sm rounded-circle ">
+                <i class="fa fa-trash"></i></button>
+        </form>
+    </th>
+</tr>
+
+
+<!--
+<div class="d-flex mb-3 w-100" style="max-width: 20rem;">
+
+    <div class="">
+        <form style="float:right" method="post" action="{{route('car.destroy', $car->id)}}" >
+                    {{ method_field('DELETE') }}
+                    {{ csrf_field() }}
+                    <button onclick="return confirm('Are you sure?')" class="btn btn-secondary btn-sm rounded-circle ">
+            <i class="fa fa-trash"></i></button>
+        </form>
+    </div>
+    <div class="">
+        <a href="/car/{{ $car->id }}">{{ $car->make }} {{ $car->model }}</a>
+        {{ $car->license_plate }}
+    </div>
+    <ul class="">
+        <li class="">
+            Issues
+            <span class="badge badge-primary badge-pill ml-auto">
+                @if($car->issues() == 0)
+                <span>0</span>
+                @elseif($car->issues() == 1)
+                <span>1</span>
+                @else
+                <span>{{$car->issues()}}</span>
+                @endif
+            </span>
+        </li>
+        <li class="">{{$car->currentDriver()}}</li>
+    </ul>
+</div>
+-->
