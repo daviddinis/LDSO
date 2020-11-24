2cd6e19fe33ebd7792fc66be349115ba48eaa5bb
david <up201706766@fe.up.pt>
2020-11-24 17:28:56 +0000

    fix badges in grid view

--- fleetmanagement/resources/views/partials/carGrid.blade.php
+++ fleetmanagement/resources/views/partials/carGrid.blade.php
@@ -2,11 +2,11 @@
 
     <div class="card-header">
         <a href="/car/{{ $car->id }}">{{ $car->make }} {{ $car->model }}</a>
-        <form style="float:right" method="post" action="{{route('car.destroy', $car->id)}}" >
-                    {{ method_field('DELETE') }}
-                    {{ csrf_field() }}
-                    <button onclick="return confirm('Are you sure?')" class="btn btn-secondary btn-sm rounded-circle ">
-            <i class="fa fa-trash"></i></button>
+        <form style="float:right" method="post" action="{{route('car.destroy', $car->id)}}">
+            {{ method_field('DELETE') }}
+            {{ csrf_field() }}
+            <button onclick="return confirm('Are you sure?')" class="btn btn-secondary btn-sm rounded-circle ">
+                <i class="fa fa-trash"></i></button>
         </form>
     </div>
     <div class="card-body">
@@ -15,15 +15,13 @@
     <ul class="list-group list-group-horizontal">
         <li class="list-group-item mr-auto">
             Issues
-            <span class="badge badge-primary badge-pill ml-auto">
-                @if($car->issues() == 0)
-                <span>0</span>
-                @elseif($car->issues() == 1)
-                <span>1</span>
-                @else
-                <span>{{$car->issues()}}</span>
-                @endif
-            </span>
+            @if($car->issues() == 0)
+            <span class="badge badge-primary badge-pill">0</span>
+            @elseif($car->issues() == 1)
+            <span class="badge badge-warning badge-pill">1</span>
+            @else
+            <span class="badge badge-danger badge-pill">{{$car->issues()}}</span>
+            @endif
         </li>
         <li class="list-group-item">{{$car->currentDriver()}}</li>
     </ul>
